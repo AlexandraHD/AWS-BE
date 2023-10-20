ac6253cd2e8dad907b572f6ff84a7161f8e45028
Alexandra Hernandez Duarte <alexa.her.duarte@gmail.com>
2023-10-20 11:19:09 -0500

    CSV parsing

--- import-service/handler.js
+++ import-service/handler.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk');
 const BUCKET = 'api-import-service';
+const cvs = require('csv-parser');
 
 module.exports.importProductsFile = async (event) => {
     try {
@@ -25,5 +26,41 @@ module.exports.importProductsFile = async (event) => {
         statusCode: 500,
         body: JSON.stringify({ error: 'URL can not be generated' }),
       };
+    }
+  };
+
+  module.exports.importProductsFile = async (event) => {
+    const s3 = new AWS.S3({ region: 'us-east-1' })
+    try {
+      for (const record of event.Records) {
+        const srcKey = record.s3.object.key;
+        const params = {
+          Bucket: BUCKET,
+          Key: srcKey,
+        };
+  
+        const s3Stream = s3.getObject(params).createReadStream();
+  
+        s3Stream
+          .pipe(csv())
+          .on('data', (data) => {
+            console.log('CSV Record:', data);
+          })
+          .on('end', () => {
+            console.log('CSV parsing finished.');
+          });
+      }
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ error: 'CSV parsing finished' }),
+      }
+
+    } catch (error) {
+      console.error('Error:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'CSV parsing failed' }),
+      }
     }
   };
